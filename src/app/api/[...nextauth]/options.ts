ab8e62d6831135a9b791e98088281f788aa35564
PKhan-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-09-19 17:29:48 +0530

    another commit

--- src/app/api/[...nextauth]/options.ts
+++ src/app/api/[...nextauth]/options.ts
@@ -39,6 +39,8 @@ export const authOptions: NextAuthOptions = {
                           const user = await userModel.findOne({
                               email: credentials.email
                           });
+                             
+                           console.log(user);
 
                             if(!user){
                                 // agar email hi nahi mila to like galt mila ya kuch bhi to error dega okkh!..
@@ -81,13 +83,82 @@ export const authOptions: NextAuthOptions = {
 
            }),
     ],
+         
+    // bass ye dhyan rakhio like ki callbacks ka hamm isliye use karte hai like session hame bydefault 
+    // 2-3 chize hi return karta hai okkh!..images email and name but id nahi karta taht's why hamm callback ka use karte hai 
+    // and  isme s jo return hua authrize s wo use ma jata hai and jwt m s hamm token m send karwa dere hai 
+    // uske badd session.user k andar sara data bhej dete hai token s okkh!.. and then hamm session use kar pate hai okkh??..
 
     callbacks: {
-        
-    }
+          // isko hamm isliye lete hai bcz jab bhi hamm call karenge like authrize wala to wha s jo bhi data hota ahi 
+          // wo session m jata hai and in case maine ye use nahi kiya hai callback ka to default name email and images return hoga 
+          // id return nahi hota okkh!..
 
+          // agr id return karwana hai to callbacks ka likhte hai and token m jata hai pehle uske badd session m jayega okkh..
+
+           async jwt({ token, user}) {
+                 
+                  // ye jo user hoga wo like jo bhi maine wha s return kiya tha authorize s wo hoga okkh!...
+                   // like id and email okkh!..
+
+                   if(user){
+                      // user barbar nahi ata ek bar hi ayega that's why mai isme koi checking nahi lagunga okkh!///
+                      token.id = user.id
+                   }
+
+                return token
+            },
+            
+            // session m data bhejne s pehle mughe token m send karna padta hai okkh!...
+            
+            async session({ session, token }){
+                 // token s jab bhi data lenge hamm to wo sara session.user m jayega okkh!..
+                  if(session.user){
+                      session.user.id = token.id as string;
+                      session.user.email = token.email as string;
+                  }
+                  console.log(session);
+                 return session;
+            },
+
+            // ek yha p hamare pass callback ka url bhi ata hai 
+            // jisse hamm decide karte hai ki wo user kha jayega aftre login??..
+
+    },
+
+
+    pages: {
+
+        // isme 5 chize ati hai bydefault.. signIn, signOut, error, newUser, verifyRequtes..
+        // to agar hamm ye pages ka section nahi likhte to smjh le ye apne app hi custimze kar leta okkh..
+        // apne app hi khud k pages bna leta but in case jo hamm yha p likhnge wo hame banane padega pages okkh!..
+        // aur generally wo frontend ka route hi likhte hai hamm jo frontend ke pages hote hai wahi..
+        
+         signIn: '/auth/sign-in',
+         error: '/auth/sign-in',
+
+          // abb ye do pages jo hai signIn and error ek page p hi ban jayenge kuch bhi galati hui to login wale page p hi wo error bhi show ho jayega okkh!..
+
+          // and jo hamne nahi banaya hai usko hamm like signOut, and newUser, verifyRequest..
+
+          
+    },
+    
+
+
+    session:{
+        strategy: 'jwt',
+        maxAge: 30*24*60*60,
+    },
+    
+    secret: process.env.NEXT_AUTH_SECRET!,
 
 }
 
 // jab bhi mai login page draw karunga to according to credentails hi karunga bcz isme s hi data aage jayega okkh!??..
 // and then wo data like credentails jo hai wo authorize m jayega fir checking hogi..
+
+
+// *** Need to remeber always *:- authorize → jwt → session..;
+
+// Yha p mughe like 2 data smj nahi aya but koi bat nahi rest chize aa gayi hai okkh! inko rat le bass ki ek session hota hai and secret hota hai..;
